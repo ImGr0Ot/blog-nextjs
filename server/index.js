6bb734a53213182cba97c480440c4df62eda2fe6
ImGr0Ot <anthonyperezpantaleon@gmail.com>
2024-04-19 22:26:04 -0400

    qwertyh

--- server/index.js
+++ server/index.js
@@ -4,5 +4,7 @@ import { connectDB } from "./db.js";
 await connectDB();
 const PORT = process.env.PORT || 4000;
 app.listen(PORT);
-
+app.get("/", (res) => {
+  res.send("Hello World!");
+});
 console.log(`Server on port${PORT}`);
