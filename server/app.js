aaf57c15919f77e93b8c48dab409ec921ecff997
ImGr0Ot <anthonyperezpantaleon@gmail.com>
2024-04-19 22:58:36 -0400

    qwedrfg

--- server/app.js
+++ server/app.js
@@ -1,17 +1,1 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import morgan from "morgan";
-import authRoutes from "./src/routes/auth.routes.js";
-import blogRoutes from "./src/routes/blogs.routes.js";
-import cors from "cors";
 
-const app = express();
-
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(cookieParser());
-app.use(authRoutes);
-app.use(blogRoutes);
-
-export default app;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
-import app from "./app.js";
 import { connectDB } from "./db.js";
+import express from "express";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import authRoutes from "./src/routes/auth.routes.js";
+import blogRoutes from "./src/routes/blogs.routes.js";
+import cors from "cors";
 
 await connectDB();
 const PORT = process.env.PORT || 4000;
-app.listen(PORT);
 
 console.log(`Server on port${PORT}`);
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(PORT);
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+app.use(authRoutes);
+app.use(blogRoutes);
+
+export default app;
